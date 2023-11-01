9e8614c1e607110033482e8c95c5dd4cf85eb550
Codesmith28 <sarthaksiddhpura@gmail.com>
2023-11-01 20:42:57 +0530

    changing the layout in the navbar

---
+++ pages/components/Groups/Groups.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Groups.module.css";
+
+function Groups() {
+  return <div>Groups</div>;
+}
+
+export default Groups;
--- pages/components/Navbar/Navbar.tsx
+++ pages/components/Navbar/Navbar.tsx
@@ -85,17 +85,16 @@ export default function Navbar() {
                 minW={0}
               >
                 <Avatar
-                  size={"sm"}
+                  size={"md"}
                   src={
                     "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
                   }
                 />
               </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
+              <MenuList minWidth="100x">
+                <>
+                  <Button>Sign Out</Button>
+                </>
               </MenuList>
             </Menu>
           </Flex>
@@ -112,7 +111,7 @@ export default function Navbar() {
         ) : null}
       </Box>
 
-      <Box p={4}>Main Content Here</Box>
+      <Box p={3}></Box>
     </>
   );
 }
