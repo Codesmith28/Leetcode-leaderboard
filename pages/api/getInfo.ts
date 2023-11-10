dc60af6f1ab7ea01e4b43e9fb2dd4ffe33f3a50c
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-11-10 12:09:55 +0530

    Add responsive grid layout and user information to ProfileCard component

---
+++ pages/api/getInfo.ts
@@ -0,0 +1,43 @@
+import { clientPromise } from "@/util/DB";
+import { MySession, UserCol } from "@/util/types";
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { decode } from "next-auth/jwt";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const sessionToken = req.cookies["next-auth.session-token"]!;
+  const token = await decode({
+    token: sessionToken,
+    secret: process.env.NEXTAUTH_SECRET!,
+  });
+
+  if (token === null) {
+    return res.status(403).send("Not logged in");
+  }
+
+  if (req.method === "GET") {
+    return GET(req, res, token as MySession["user"]);
+  } else {
+    return res.status(405).send("Method not allowed");
+  }
+}
+
+async function GET(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session: MySession["user"]
+) {
+  const db = (await clientPromise).db("leetcodeleaderboard");
+  const usersCollection = db.collection<UserCol>("Users");
+  const id = session.id;
+
+  const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+  if (!user) {
+    return res.status(500).json({ error: "Could not find user" });
+  }
+
+  return res.status(200).json({ username: user.username });
+}
