6a3c19176f2c81f2eda69a5d31648016f872d664
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-11-24 12:16:55 +0530

    Add TeamPlate component to MyTeams page

--- pages/Member/MyTeams/[teamId]/index.tsx
+++ pages/Member/MyTeams/[teamId]/index.tsx
@@ -1,16 +1,18 @@
 import Layout from "@/pages/Layout";
+import TeamPlate from "@/pages/components/TeamPlate/TeamPlate";
+import { ObjectId } from "mongodb";
 import { useRouter } from "next/router";
 import React from "react";
 import styles from "./CurrTeam.module.css";
 
 function index() {
   const route = useRouter();
-  const { teamId } = route.query;
+  const teamId = route.query.teamId as string;
   console.log("route = ", teamId);
   return (
     <>
       <Layout>
-        <div>index</div>
+        <TeamPlate teamId={teamId} />
       </Layout>
     </>
   );
--- pages/components/TeamPlate/TeamPlate.tsx
+++ pages/components/TeamPlate/TeamPlate.tsx
@@ -1,8 +1,52 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { ObjectId } from "mongodb";
 import React from "react";
 import styles from "./TeamPlate.module.css";
 
-function TeamPlate() {
-  return <div>TeamPlate</div>;
-}
+function TeamPlate({ teamId }: { teamId: string } ) {
+  let mem = 10;
 
+  return (
+    <>
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+        className={styles.mainCard}
+      >
+        <div className={styles.plateHead}>
+          <Heading size={"lg"}>Team Name</Heading>
+
+          <Heading size={"md"} color={"gray"}>
+            Institution
+          </Heading>
+        </div>
+
+        <div>
+          {/* total members */}
+          <Heading size={"sm"}>Total Members : {mem}</Heading>
+          <div className={styles.topThree}>
+            <Heading size={"sm"} color={"gold"} textDecor={"underline"}>
+              #1
+            </Heading>
+            <Heading size={"sm"} color={"silver"} textDecor={"underline"}>
+              #2
+            </Heading>
+            <Heading size={"sm"} color={"brown"} textDecor={"underline"}>
+              #3
+            </Heading>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+}
 export default TeamPlate;
