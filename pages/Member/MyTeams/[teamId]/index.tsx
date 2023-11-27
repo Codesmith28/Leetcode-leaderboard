7817d706c327e209b7a55d40c6ac8697f70653fd
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-11-27 10:32:28 +0530

    Update user info and team info retrieval

--- pages/Member/MyTeams/[teamId]/index.tsx
+++ pages/Member/MyTeams/[teamId]/index.tsx
@@ -32,6 +32,18 @@ interface userInfo {
   image: string;
 }
 
+interface Info {
+  image: string;
+  username: string;
+  email: string;
+  institution: string;
+  totalSolved: number;
+  easySolved: number;
+  mediumSolved: number;
+  hardSolved: number;
+  ranking: number;
+}
+
 function index() {
   const route = useRouter();
   const teamId = route.query.teamId as string;
@@ -44,7 +56,72 @@ function index() {
     disabled: false,
   });
 
+  const [userInfo, setUserInfo] = useState<Info>({
+    image: "",
+    username: "",
+    email: "",
+    institution: "",
+    totalSolved: 0,
+    easySolved: 0,
+    mediumSolved: 0,
+    hardSolved: 0,
+    ranking: 0,
+  });
+
   useEffect(() => {
+    // to update the info of the user:
+    const upDateInfo = async () => {
+      const res = await fetch("/api/getInfo", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+
+      const res2 = await fetch(
+        `https://leetcode-api-faisalshohag.vercel.app/${data.username}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data2 = await res2.json();
+
+      // combine the data from the two api calls:
+      const combinedInfo: Info = {
+        image: data.image,
+        username: data.username,
+        email: data.email,
+        institution: data.institution,
+        totalSolved: data2.totalSolved,
+        easySolved: data2.easySolved,
+        mediumSolved: data2.mediumSolved,
+        hardSolved: data2.hardSolved,
+        ranking: data2.ranking,
+      };
+      setUserInfo(combinedInfo);
+
+      // update the database with the data2 in the database:
+      const res3 = await fetch("/api/updateInfo", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          easySolved: data2.easySolved,
+          mediumSolved: data2.mediumSolved,
+          hardSolved: data2.hardSolved,
+          totalSolved: data2.totalSolved,
+          ranking: data2.ranking,
+        }),
+      });
+      const data3 = await res3.json();
+    };
+    upDateInfo();
+
     if (teamId) {
       const getTeamInfo = async () => {
         const res = await fetch(`/api/getTeamInfo/${teamId}`, {
@@ -56,6 +133,7 @@ function index() {
         const teamData = await res.json();
         setteamInfo(teamData);
       };
+
       getTeamInfo();
     }
   }, [teamId]);
