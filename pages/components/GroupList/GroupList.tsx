3b0017a186ebf55e6673952ed9a852055fe506ce
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-08 18:50:50 +0530

    trying to fix

--- pages/components/GroupList/GroupList.tsx
+++ pages/components/GroupList/GroupList.tsx
@@ -1,31 +1,32 @@
+import { TeamData } from "@/util/types";
 import Groups from "../Groups";
 import styles from "./GroupList.module.css";
 
 export default function GroupList({
-  teamData,
-  myInsti,
+	teamData,
+	myInsti,
 }: {
-  teamData: any;
-  myInsti: string;
+	teamData: TeamData[];
+	myInsti: string;
 }) {
-  const transition = {
-    duration: 0.3,
-    ease: "easeInOut",
-  };
+	const transition = {
+		duration: 0.3,
+		ease: "easeInOut",
+	};
 
-  return (
-    <div className={styles.groups}>
-      {teamData.map((group: any, index: number) => (
-        <Groups
-          key={index}
-          _id={group._id}
-          institution={group.institution}
-          name={group.name}
-          totalMembers={group.totalMembers}
-          disabled={myInsti !== group.institution}
-          transition={{ ...transition, delay: index * 0.09 }}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className={styles.groups}>
+			{teamData.map((group, index: number) => (
+				<Groups
+					key={index}
+					_id={group._id}
+					institution={group.institution}
+					name={group.name}
+					totalMembers={group.totalMembers}
+					disabled={myInsti !== group.institution}
+					transition={{ ...transition, delay: index * 0.09 }}
+				/>
+			))}
+		</div>
+	);
 }
--- util/types.ts
+++ util/types.ts
@@ -2,42 +2,49 @@ import { ObjectId } from "mongodb";
 export type Role = "Admin" | "Member";
 
 export type UserCol = {
-  name: string;
-  email: string;
-  username?: string;
-  institution?: string;
-  LCTotalSolved?: number;
-  LCEasySolved?: number;
-  LCMediumSolved?: number;
-  LCHardSolved?: number;
-  role: Role;
-  _id?: ObjectId;
-  teams: ObjectId[];
-  ranking: number;
-  image: string;
+	name: string;
+	email: string;
+	username?: string;
+	institution?: string;
+	LCTotalSolved?: number;
+	LCEasySolved?: number;
+	LCMediumSolved?: number;
+	LCHardSolved?: number;
+	role: Role;
+	_id?: ObjectId;
+	teams: ObjectId[];
+	ranking: number;
+	image: string;
 };
 
 export type MySession = {
-  user: {
-    id: ObjectId;
-    username: string;
-    role: Role;
-    name: string;
-    email: string;
-    image: string;
-    institution?: string;
-    LCTotalSolved?: number;
-    LCEasySolved?: number;
-    LCMediumSolved?: number;
-    LCHardSolved?: number;
-    teams: ObjectId[];
-    ranking: number;
-  };
+	user: {
+		id: ObjectId;
+		username: string;
+		role: Role;
+		name: string;
+		email: string;
+		image: string;
+		institution?: string;
+		LCTotalSolved?: number;
+		LCEasySolved?: number;
+		LCMediumSolved?: number;
+		LCHardSolved?: number;
+		teams: ObjectId[];
+		ranking: number;
+	};
 };
 
 export type TeamCol = {
-  _id?: ObjectId;
-  name: string;
-  members: ObjectId[];
-  institution: string | null;
+	_id: ObjectId;
+	name: string;
+	members: ObjectId[];
+	institution: string | null;
 };
+
+export type TeamData = Omit<
+	TeamCol & {
+		totalMembers: number;
+	},
+	"members"
+>;
