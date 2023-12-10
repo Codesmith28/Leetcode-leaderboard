8a8bcce90641db9ff4bff20ffc813bdc71f1acad
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-10 09:54:12 +0530

    Update team member information and fetch data from LeetCode API (something is wrong i can feel it)

--- pages/Member/MyTeams/[teamId]/index.tsx
+++ pages/Member/MyTeams/[teamId]/index.tsx
@@ -1,6 +1,6 @@
-import Layout from "@/pages/Layout";
 import TeamPlate from "@/components/TeamPlate/TeamPlate";
 import UserList from "@/components/UserList/UserList";
+import Layout from "@/pages/Layout";
 import { Role, UserCol } from "@/util/types";
 import { Divider } from "@chakra-ui/react";
 import { ObjectId } from "mongodb";
@@ -9,146 +9,151 @@ import React, { useEffect, useState } from "react";
 import styles from "./CurrTeam.module.css";
 
 interface teamInfo {
-	name: string;
-	institution: string;
-	totalMembers: number;
-	members: userInfo[];
-	disabled: boolean;
+  name: string;
+  institution: string;
+  totalMembers: number;
+  members: userInfo[];
+  disabled: boolean;
 }
 
 interface userInfo {
-	_id: ObjectId;
-	name: string;
-	username: string;
-	email: string;
-	institution: string;
-	totalSolved: number;
-	easySolved: number;
-	mediumSolved: number;
-	hardSolved: number;
-	ranking: number;
-	role: Role;
-	teams: ObjectId[];
-	image: string;
+  _id: ObjectId;
+  name: string;
+  username: string;
+  email: string;
+  institution: string;
+  totalSolved: number;
+  easySolved: number;
+  mediumSolved: number;
+  hardSolved: number;
+  ranking: number;
+  role: Role;
+  teams: ObjectId[];
+  image: string;
 }
 
 interface Info {
-	image: string;
-	username: string;
-	email: string;
-	institution: string;
-	totalSolved: number;
-	easySolved: number;
-	mediumSolved: number;
-	hardSolved: number;
-	ranking: number;
+  image: string;
+  username: string;
+  email: string;
+  institution: string;
+  totalSolved: number;
+  easySolved: number;
+  mediumSolved: number;
+  hardSolved: number;
+  ranking: number;
 }
 
 function index() {
-	const route = useRouter();
-	const teamId = route.query.teamId as string;
+  const route = useRouter();
+  const teamId = route.query.teamId as string;
 
-	const [teamInfo, setteamInfo] = useState<teamInfo>({
-		name: "",
-		institution: "",
-		totalMembers: 0,
-		members: [],
-		disabled: false,
-	});
+  // fetch data from leetcode api for all users and update it in the database:
 
-	const [userInfo, setUserInfo] = useState<Info>({
-		image: "",
-		username: "",
-		email: "",
-		institution: "",
-		totalSolved: 0,
-		easySolved: 0,
-		mediumSolved: 0,
-		hardSolved: 0,
-		ranking: 0,
-	});
+  const [teamInfo, setteamInfo] = useState<teamInfo>({
+    name: "",
+    institution: "",
+    totalMembers: 0,
+    members: [],
+    disabled: false,
+  });
 
-	useEffect(() => {
-		// to update the info of the user:
-		const upDateInfo = async () => {
-			const res = await fetch("/api/getInfo", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			const data = await res.json();
+  const [userInfo, setUserInfo] = useState<Info>({
+    image: "",
+    username: "",
+    email: "",
+    institution: "",
+    totalSolved: 0,
+    easySolved: 0,
+    mediumSolved: 0,
+    hardSolved: 0,
+    ranking: 0,
+  });
 
-			const res2 = await fetch(
-				`https://leetcode-api-faisalshohag.vercel.app/${data.username}`,
-				{
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
-			);
-			const data2 = await res2.json();
+  useEffect(() => {
+    // update information of all members of the team:
+    const updateMembersInfo = async () => {
+      // Iterate through each member in the teamInfo.members array
+      for (const member of teamInfo.members) {
+        // Make an API call to fetch the member's information from the LeetCode API
+        const res = await fetch(
+          `https://leetcode-api-faisalshohag.vercel.app/${member.username}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const memberData = await res.json();
 
-			// combine the data from the two api calls:
-			const combinedInfo: Info = {
-				image: data.image,
-				username: data.username,
-				email: data.email,
-				institution: data.institution,
-				totalSolved: data2.totalSolved,
-				easySolved: data2.easySolved,
-				mediumSolved: data2.mediumSolved,
-				hardSolved: data2.hardSolved,
-				ranking: data2.ranking,
-			};
-			setUserInfo(combinedInfo);
+        // Update the member's information in the teamInfo.members array
+        const updatedMembers = teamInfo.members.map((m) => {
+          if (m._id === member._id) {
+            return {
+              ...m,
+              totalSolved: memberData.totalSolved,
+              easySolved: memberData.easySolved,
+              mediumSolved: memberData.mediumSolved,
+              hardSolved: memberData.hardSolved,
+              ranking: memberData.ranking,
+            };
+          }
+          return m;
+        });
 
-			// update the database with the data2 in the database:
-			const res3 = await fetch("/api/updateInfo", {
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					easySolved: data2.easySolved,
-					mediumSolved: data2.mediumSolved,
-					hardSolved: data2.hardSolved,
-					totalSolved: data2.totalSolved,
-					ranking: data2.ranking,
-				}),
-			});
-			const data3 = await res3.json();
-		};
-		upDateInfo();
+        // Update the teamInfo state with the updated members information
+        setteamInfo((prevState) => ({
+          ...prevState,
+          members: updatedMembers,
+        }));
 
-		if (teamId) {
-			const getTeamInfo = async () => {
-				const res = await fetch(`/api/getTeamInfo/${teamId}`, {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				});
-				const teamData = await res.json();
-				setteamInfo(teamData);
-			};
+        // Update the member's information in the database using the updateInfo API
+        await fetch("/api/updateInfo", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            _id: member._id,
+            easySolved: memberData.easySolved,
+            mediumSolved: memberData.mediumSolved,
+            hardSolved: memberData.hardSolved,
+            totalSolved: memberData.totalSolved,
+            ranking: memberData.ranking,
+          }),
+        });
+      }
+    };
 
-			getTeamInfo();
-		}
-	}, [teamId]);
+    if (teamId) {
+      const getTeamInfo = async () => {
+        const res = await fetch(`/api/getTeamInfo/${teamId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const teamData = await res.json();
+        setteamInfo(teamData);
+      };
 
-	const topThree = teamInfo.members.slice(0, 3);
+      updateMembersInfo();
+      getTeamInfo();
+    }
+  }, [teamId]);
 
-	return (
-		<>
-			<Layout>
-				<TeamPlate teamInfo={teamInfo} topThree={topThree} />
-				<Divider marginBlock={"1em"} />
-				<UserList members={teamInfo.members} />
-			</Layout>
-		</>
-	);
+  const topThree = teamInfo.members.slice(0, 3);
+
+  return (
+    <>
+      <Layout>
+        <TeamPlate teamInfo={teamInfo} topThree={topThree} />
+        <Divider marginBlock={"1em"} />
+        <UserList members={teamInfo.members} />
+      </Layout>
+    </>
+  );
 }
 
 export default index;
