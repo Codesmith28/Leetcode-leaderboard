655c6d0221bbc9714f30e614a95f45a03dd6a71a
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-12-12 18:57:33 +0530

    Refactor leaveTeam API to use bulkWrite

--- pages/api/leaveTeam.ts
+++ pages/api/leaveTeam.ts
@@ -34,23 +34,60 @@ async function PUT(
   const userCollection = db.collection<UserCol>("Users");
   const teamCollection = db.collection<TeamCol>("Teams");
 
-  const result = await teamCollection.updateOne(
-    { _id: new ObjectId(teamId) },
+  // const result = await teamCollection.updateOne(
+  //   { _id: new ObjectId(teamId) },
+  //   {
+  //     $pull: { members: new ObjectId(userId) },
+  //   }
+  // );
+
+  // if (result.matchedCount > 0) {
+  //   await userCollection.updateOne(
+  //     { _id: new ObjectId(userId) },
+  //     {
+  //       $pull: { teams: new ObjectId(teamId) },
+  //     }
+  //   );
+
+  //   return res.status(200).json({ message: "User removed from the team" });
+  // } else {
+  //   return res.status(404).send("User or team not found");
+  // }
+
+  const operation1 = [
     {
-      $pull: { members: new ObjectId(userId) },
-    }
-  );
+      updateOne: {
+        filter: { _id: new ObjectId(teamId) },
+        update: {
+          $pull: { members: new ObjectId(userId) },
+        },
+      },
+    },
+  ];
 
-  if (result.matchedCount > 0) {
-    await userCollection.updateOne(
-      { _id: new ObjectId(userId) },
-      {
-        $pull: { teams: new ObjectId(teamId) },
-      }
-    );
+  const operation2 = [
+    {
+      updateOne: {
+        filter: { _id: new ObjectId(userId) },
+        update: {
+          $pull: { teams: new ObjectId(teamId) },
+        },
+      },
+    },
+  ];
 
+  console.time("one");
+  const res1 = await teamCollection.bulkWrite(operation1);
+  console.timeEnd("one");
+
+  console.time("two");
+  const res2 = await userCollection.bulkWrite(operation2);
+  console.timeEnd("two");
+
+  if (res1.modifiedCount > 0 && res2.modifiedCount > 0) {
     return res.status(200).json({ message: "User removed from the team" });
   } else {
     return res.status(404).send("User or team not found");
   }
 }
+  
