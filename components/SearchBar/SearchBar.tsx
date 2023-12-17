9b4c3281e7320dbc0d9375461e89695584c49914
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-12-17 12:13:28 +0530

    Refactor SearchBar component

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,39 +1,40 @@
 import { Search2Icon } from "@chakra-ui/icons";
 import {
-  FormControl,
-  Input,
-  InputGroup,
-  InputRightElement,
+	FormControl,
+	Input,
+	InputGroup,
+	InputRightElement,
 } from "@chakra-ui/react";
 import React from "react";
 import styles from "./SearchBar.module.css";
 
 interface SearchBarProps {
-  searchQuery: string;
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
+	searchQuery: string;
+	setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+	setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 function SearchBar({ searchQuery, setSearchQuery, setPage }: SearchBarProps) {
-  return (
-    <div className={styles.searchBar}>
-      <FormControl id="search">
-        <InputGroup>
-          <InputRightElement pointerEvents="none">
-            <Search2Icon color="gray.300" />
-          </InputRightElement>
-          <Input
-            type="search"
-            placeholder="Search Teams"
-            onChange={(e) => {
-              setSearchQuery(e.target.value);
-              setPage(1);
-            }}
-          />
-        </InputGroup>
-      </FormControl>
-    </div>
-  );
+	return (
+		<div className={styles.searchBar}>
+			<FormControl id="search">
+				<InputGroup>
+					<InputRightElement pointerEvents="none">
+						<Search2Icon color="gray.300" />
+					</InputRightElement>
+					<Input
+						type="search"
+						placeholder="Search Teams"
+						value={searchQuery}
+						onChange={(e) => {
+							setSearchQuery(e.target.value);
+							setPage(1);
+						}}
+					/>
+				</InputGroup>
+			</FormControl>
+		</div>
+	);
 }
 
 export default SearchBar;
